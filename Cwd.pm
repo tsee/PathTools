3028dda206ad166c4427ea2bb971dd66eb1a748b
Ken Williams <kwilliams@cpan.org>
2006-03-03 22:52:37 +0000

    Version bump

--- Cwd.pm
+++ Cwd.pm
@@ -171,7 +171,7 @@ use strict;
 use Exporter;
 use vars qw(@ISA @EXPORT @EXPORT_OK $VERSION);
 
-$VERSION = '3.16';
+$VERSION = '3.17';
 
 @ISA = qw/ Exporter /;
 @EXPORT = qw(cwd getcwd fastcwd fastgetcwd);
