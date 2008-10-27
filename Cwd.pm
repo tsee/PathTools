39988c77e4dc547ff4ad6076541d5ab864098b55
Steffen Mueller <smueller@cpan.org>
2008-10-27 21:14:26 +0000

    Update version to 3.28_03 for another dev release.

--- Cwd.pm
+++ Cwd.pm
@@ -171,7 +171,7 @@ use strict;
 use Exporter;
 use vars qw(@ISA @EXPORT @EXPORT_OK $VERSION);
 
-$VERSION = '3.28_02';
+$VERSION = '3.28_03';
 my $xs_version = $VERSION;
 $VERSION = eval $VERSION;
 
